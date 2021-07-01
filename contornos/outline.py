1442f883258ba6cab91aa4623d84a91bb452b34c
Kenyi Hancco <kenyihq@gmail.com>
2021-07-01 18:49:03 -0500

    Se inicia el proyecto, creamos nuestro entorno virtual, instalamos la librería OpenCV, y empezamos a escribir nusetras primeras líneas

---
+++ contornos/outline.py
@@ -0,0 +1,15 @@
+import cv2 # Importamos a OpenCV
+
+# Capturamos la imagen con una ruta relativa
+img = cv2.imread('contorno.jpg')
+# Convertimos la imagen a grises
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Capturamos el Umbral de la imagen ('_' variable ficticia)
+_, umbral = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+
+# Mostramos la imagen en una ventana
+cv2.imshow('Imagen Original', img)
+cv2.imshow('Imagen en grises', gray)
+cv2.imshow('Imagen con umbral', umbral)
+cv2.waitKey(0) # 1 dinamicom 0 estatico
+cv2.destroyAllWindows() # Cierra todas las ventanas abiertas
